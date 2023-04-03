1473676c5b4dd6ff86f752190e7531aadd3edb07
Irfan2309 <is2012@hw.ac.uk>
2023-04-03 23:57:45 +0400

    forgot to save before pushing last commit

--- src/DrillLoginManager.java
+++ src/DrillLoginManager.java
@@ -10,6 +10,7 @@ import java.util.concurrent.locks.Condition; //Note that the 'notifyAll' method 
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 
 //IMPORTANT: DrillLoginManager must handle exceptions locally i.e. it must not  explicitly 'throw' exceptions 
@@ -23,8 +24,9 @@ public class DrillLoginManager implements Manager {
 	private final Condition workerCondition = lock.newCondition();
 	
 	/* making a map that will store all the team that makes the request and their worker requirements 
-	 * the map stores the team name as the key and the value is another map that stores the worker role and the number of workers */
-	private final Map<String, Map<String, Integer>> requestsMade = new HashMap<>();
+	 * the map stores the team name as the key and the value is another map that stores the worker role and the number of workers 
+	 * Using LinkedHashMap to have an order in which the requests enter the map. (so we can use FIFO) */
+	private final Map<String, Map<String, Integer>> requestsMade = new LinkedHashMap<>();
 	
 	@Override
 	public void smallTeamRequest(Map<String, Integer> team) {
@@ -64,8 +66,9 @@ public class DrillLoginManager implements Manager {
 		//lock protection
 		lock.lock();
 		try {
-			/* check if worker is needed in any requests (while?)
-			 * if yes, then add them to the team (decrement from the role count needed in the team?)			
+			/* check if any requests are available (check requestsMade) 
+			 * check if worker is needed in any requests 
+			 * if yes, then add them to the team		
 			 * if no, block using await (will be woken up the signals in the request methods)
 			 * */
 			return null;
@@ -75,7 +78,6 @@ public class DrillLoginManager implements Manager {
 		}	
 	}
 	
-	//Note that you may add inner classes and methods to this file as shown below:
 	private class proceedRequest {
 		/*once the role requirement is fulfilled, the thread should proceed 
 		 * i.e. the request should no longer be available (remove from map requestsMade) */
